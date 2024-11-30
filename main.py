fb054ded22be2c16f0c81586bb1ce39b95ae7db5
Marcus Sena <mens.eai20@uea.edu.br>
2024-11-30 11:35:32 -0400

    Beta Version - It works - Post Method Implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse, JSONResponse
-# from utils import my_module
+from utils import my_module
 import os
 from datetime import datetime
 
@@ -10,8 +10,6 @@ app = FastAPI()
 # Root endpoint
 @app.get("/image")
 def read_image():
-    # my_module.ai_api()
-
     image_path = "utils/input/teste.jpeg"
 
     # Check if the file exists
@@ -30,15 +28,46 @@ async def upload_image(file: UploadFile = File(...)):
         # Generate a timestamp-based filename
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
         file_extension = file.filename.split('.')[-1]  # Get file extension
-        file_path = f'./utils/input/uploaded_image_{timestamp}.{file_extension}'
+        file_name = f'uploaded_image_{timestamp}.{file_extension}'
+        file_path = f'./utils/input/{file_name}'
         
         # Save the file with the new filename
         with open(file_path, "wb") as image_file:
             image_file.write(await file.read())
+
+        model_path = r'./utils/best.pt'
+        output_dir = r'./utils/output/'
+
+        my_module.ai_api(file_path, model_path)
         
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        base_path = "runs/segment/"
+        last_path = get_last_alphabetical_dir(base_path)
+        # complete_path = f'{base_path}{last_path}/'
+        image_name_and_extension = get_single_file_name(last_path) 
+        image_full_path = f'{last_path}/{image_name_and_extension}'
+    
         # Return the uploaded image file as a response
-        return FileResponse(file_path, media_type=f"image/{file_extension}", headers={"Content-Disposition": f"attachment; filename={os.path.basename(file_path)}"})
+        return FileResponse(image_full_path, media_type=f"image/{file_extension}", headers={"Content-Disposition": f"attachment; filename={os.path.basename(image_full_path)}"})
     
     except Exception as e:
         print(f"Error during file upload: {e}")  # Log the error
         return JSONResponse(status_code=400, content={"message": str(e)})
+
+def get_last_alphabetical_dir(base_path):
+    dirs = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
+    if dirs:
+        last_dir = sorted(dirs)[-1]  # Sort alphabetically and get the last one
+        return os.path.join(base_path, last_dir)
+    return None
+
+
+def get_single_file_name(folder_path):
+    # List all items in the folder
+    items = os.listdir(folder_path)
+    # Filter for files only
+    files = [item for item in items if os.path.isfile(os.path.join(folder_path, item))]
+    # Check if there's exactly one file
+    return files[0]  # Return the single file name
--- utils/my_module.py
+++ utils/my_module.py
@@ -1,10 +1,15 @@
 import cv2
+import os
 from ultralytics import YOLO
 
-
-model_path = r'./best.pt'
-
-model = YOLO(model_path)
-
-def ai_api(image_path):
-    model.predict(source=image_path, show =True, save =True, show_labels=True,show_conf=False,conf=0.2,save_txt=False)
+def ai_api(image_path, model_path):
+    model = YOLO(model_path)
+    model.predict(
+        source=image_path, 
+        show =False, 
+        save =True,
+        show_labels=True,
+        show_conf=False,
+        conf=0.2,
+        save_txt=False
+        )
